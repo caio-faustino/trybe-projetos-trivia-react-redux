a86a5e366ec5b4fb7a8295c9a00a595b878260d4
Felipe-Nakano <felipenakano04@gmail.com>
2023-03-21 18:24:16 -0300

    dando início ao requisito 14

---
+++ src/components/FinalFeedback.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class FinalFeedback extends React.Component {
+  render() {
+    return (<div />);
+  }
+}
+
+export default FinalFeedback;
